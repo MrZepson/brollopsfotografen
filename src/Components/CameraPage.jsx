96eac419395e4741202bf65f9a8d49df7fa51a32
Daniel Häggström <danielhaggstrom@hotmail.se>
2022-05-28 00:07:00 +0200

    saving picture progress

--- src/Components/CameraPage.jsx
+++ src/Components/CameraPage.jsx
@@ -1,9 +1,12 @@
 import styles from "./CameraPage.module.css";
 import cameraIcon from "../img/camera.svg";
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 
 export default function CameraPage() {
+  const [showVideo, setShowVideo] = useState(true);
+
   const videoRef = useRef(null);
+  const photoRef = useRef(null);
 
   useEffect(() => {
     const getUserMedia = async () => {
@@ -20,12 +23,26 @@ export default function CameraPage() {
   }, []);
 
   function getPicture() {
-    return;
+    const ctx = photoRef.current.getContext("2d");
+    ctx.drawImage(
+      videoRef.current,
+      0,
+      0,
+      photoRef.current.width,
+      photoRef.current.height
+    );
+    const imageData = photoRef.current.toDataURL("image/png");
+    console.log(imageData);
+    setShowVideo(!showVideo);
   }
 
   return (
     <main className={styles.wrapper}>
       <video className={styles.camera} ref={videoRef} autoPlay />
+      <canvas
+        style={showVideo ? { display: "none" } : { display: "block" }}
+        ref={photoRef}
+      ></canvas>
       <div className={styles.barBottom}>
         <button onClick={getPicture} className={styles.button}>
           <img
